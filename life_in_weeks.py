e00542fb09700517e5b72037bf1a12de9b169b6f
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-02-07 14:32:15 -0600

    Day 2 life in weeks

---
+++ life_in_weeks.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+diff = 90 - int(age)
+
+days = diff * 365
+weeks = diff * 52
+months = diff * 12
+
+print(f"you have {days} days, {weeks} weeks, and {months} months left.")
