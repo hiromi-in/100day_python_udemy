22444db47d68aa96c4bb5bde14468ed842f0b70f
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-02-12 13:54:35 -0600

    Day5 High Score v2

--- high_score.py
+++ high_score.py
@@ -16,5 +16,20 @@ for n in student_scores:
 print(f'The highest score in the class is: {highest}')
 
 
+------------------------------------------------------------
 
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+highest = 0
+for n in student_scores:
+    if highest < n:
+        highest = n
+
+print(f'The highest score in the class is: {highest}')
 
