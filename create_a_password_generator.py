0a90a048a7fa71662b77fdbad16280739df51bac
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-02-12 15:10:49 -0600

    Day5 project  (Add sample solution code)

--- create_a_password_generator.py
+++ create_a_password_generator.py
@@ -38,3 +38,28 @@ for num in range(1, nr_numbers+1):
   password = password[:place] + random.choice(numbers) + password[place:]
   
 print(f'Here is your password: {password}')
+
+
+
+
+---------------------------------------------------
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
