9cb5c08cb12e84b4914cc89dee43f41175cd35d6
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-02-13 14:01:58 -0600

    Day 5 Hurdle1

---
+++ hurdle1.py
@@ -0,0 +1,25 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def to_the_right():
+    turn_right()
+    move()
+
+def one_hurdle():
+    move()
+    turn_left()
+    move()
+    to_the_right()
+    to_the_right()
+    turn_left()
+
+for i in range(1,7):
+    one_hurdle()
+
+
+
+
