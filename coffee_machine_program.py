4111339a2f1896066a719f9e427098971553ac27
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-03-02 11:50:16 -0600

    Day15 Coffee Machine Program
    Modified resource_match() to change the location of the function.

--- coffee_machine_program.py
+++ coffee_machine_program.py
@@ -1,49 +1,32 @@
 from menu import MENU, resources
 
-resources["money"] = 0
-
 water = resources["water"]
 milk = resources["milk"]
 coffee = resources["coffee"]
-money = resources["money"]
 
-quarters = 0
-dimes = 0
-nickles = 0
-pennies = 0
+profit = 0
+total = 0
+
 
 def coins():
-    global quarters
-    global dimes
-    global nickles
-    global pennies
+    global total
     print("Please insert coins.")
-    quarters = 0.25 * float(input("How many quarters?"))
-    dimes = 0.10 * float(input("How many dimes?"))
-    nickles = 0.05 * float(input("How many nickles?"))
-    pennies = 0.01 * float(input("How many pennies?"))
+    total = 0.25 * float(input("How many quarters?"))
+    total += (0.10 * float(input("How many dimes?")))
+    total += (0.05 * float(input("How many nickles?")))
+    total += (0.01 * float(input("How many pennies?")))
+    return total
+
 
 def resource_match(drink):
-    global water
-    global milk
-    global coffee
-    global money
-    global change
-    for ingredient in resources:
-        if ingredient not in MENU[drink]["ingredients"]:
-            MENU[drink]["ingredients"][ingredient] = 0
-    water -= MENU[drink]["ingredients"]["water"]
-    milk -= MENU[drink]["ingredients"]["milk"]
-    coffee -= MENU[drink]["ingredients"]["coffee"]
-    if water < 0:
-        print("Sorry, there is not enough water.")
-    elif milk < 0:
-        print("Sorry, there is not enough milk.")
-    elif coffee < 0:
-        print("Sorry, there is not enough coffee.")
-    else:
-        money += cost
-        print(f"Here is ${change} in change.\nHere is your {order}☕. Enjoy!")
+    global order_continue
+    for i in resources:
+        if MENU[drink]["ingredients"][i] > resources[i]:
+            print(f"Sorry there is not enough {i}.")
+            order_continue = False
+        else:
+            resources[i] -= MENU[drink]["ingredients"][i]
+
 
 order_continue = True
 
@@ -51,26 +34,31 @@ while order_continue:
     order = input("What would you like? (espresso/latte/cappuccino)")
 
     if order == 'report':
-        print(f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${money}")
+        print(f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${profit}")
     elif order == 'espresso' or order == 'latte' or order == 'cappuccino':
+        resource_match(drink=order)
+        if order_continue == False:
+            print("Please come back later. Good bye!")
+            break
         cost = MENU[order]["cost"]
-        coins()
-        paid = round(quarters + dimes + nickles + pennies, 2)
+        paid = coins()
         if paid < cost:
             print("Sorry that's not enough money. Money refunded.")
         elif paid >= cost:
-            change = paid - cost
-            resource_match(drink = order)
+            change = round(paid - cost, 2)
+            profit += cost
+            print(f"Here is ${change} in change.\nHere is your {order}☕. Enjoy!")
     elif order == 'off':
         print("Turning off. Good bye!")
-        order_continue = False
+        break
     else:
         print("Wrong order. Good bye!")
-        order_continue = False
+        break
 
-    if input("Would you like another drink? Type 'y' for yes, 'n' for no." ) == 'n':
+    if input("Would you like another drink? Type 'y' for yes, 'n' for no.") == 'n':
         print("Have a good day!😄")
         order_continue = False
+
 
         ----------------------------------------------------------------------
         menu.py
