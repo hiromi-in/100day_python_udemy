17be53559ef0175eedcc7f3a4193480f40f0c313
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-03-20 16:23:20 -0500

    Day26 Data Overlap v2

--- data_overlap.py
+++ data_overlap.py
@@ -49,3 +49,26 @@ with open("file1.txt") as file1:
 print(result)
 
 
+---------------------------------------------
+v2
+
+
+with open("file1.txt") as file1:
+    file1_list = file1.readlines()
+    #print(file1_list)
+    with open("file2.txt") as file2:
+        file2_list = file2.readlines()
+        #print(file2_list)
+        result = [int(num) for num in file1_list if num in file2_list]
+
+
+
+
+
+# Write your code above 👆
+
+print(result)
+
+
+
+
