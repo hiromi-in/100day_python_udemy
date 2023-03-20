e41e85e38164d602c90863519fe5fa462e92cb54
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-03-20 17:14:52 -0500

    Day26 Dictionary Comprehension v2

--- dictionary_comprehension_1.py
+++ dictionary_comprehension_1.py
@@ -9,3 +9,11 @@ result = {word:len(word) for word in sentence_list}
 
 print(result)
 
+--------------------------------------------
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+result = {word:len(word) for word in sentence.split()}
+
+print(result)
